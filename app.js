bcbef4e3f620f8cb39663ee7f4580625b6fdc5ac
Yeskermes <91124633+Yeskermes@users.noreply.github.com>
2021-11-29 01:11:58 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function myFunction1() {
+  var x = document.getElementById("myInput1");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function myFunction2() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+//
